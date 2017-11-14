4d9bcd418a0f064f2f1cce7f5c97bbdd1b125a22
CalvinBerrett <31518724+CalvinBerrett@users.noreply.github.com>
2017-11-14 12:36:57 -0700

    added code for setting secondLabel and imageOne

--- CalvinBerrett_CSP/Controller/GlobalImpactScreen.swift
+++ CalvinBerrett_CSP/Controller/GlobalImpactScreen.swift
@@ -10,14 +10,24 @@ import UIKit
 
 class GlobalImpactScreen: UIViewController
 {
-
+    @IBOutlet weak var secondLabel: UILabel!
+    @IBOutlet weak var thirdLabel: UILabel!
+    @IBOutlet weak var imageOne: UIImageView!
     override func viewDidLoad()
     {
         super.viewDidLoad()
-
+        setup()
         // Do any additional setup after loading the view.
+
     }
 
+    private func setup() -> Void
+    {
+        secondLabel.text = "Water is essential for everybody, and the dirty water that a lot of people currently drink harms their health. So we need to get clean water for people to live healthier lives"
+        imageOne.image = #imageLiteral(resourceName: "AppGUI")
+        thirdLabel.text = ""
+        
+    }
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
